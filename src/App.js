7a1066a6e9a9dbf8da0f51918843b543d23cb8b0
yaodingyd <yaodingyd@gmail.com>
2018-01-02 15:00:40 -0500

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="container">
         <section className="item discover">
-          <img src="https://creditcards.chase.com/R103-011/1030210/images/32616_ChaseFreedomCard_Sticky.png" />
+          <img src="https://creditcards.chase.com/R103-011/1030210/images/32616_ChaseFreedomCard_Sticky.png" alt="discover" />
           <ul>
             { Categories.freedom.map((category) =>
               <li className="category">{category}</li>
@@ -15,7 +15,7 @@ class App extends Component {
           </ul>
         </section>
         <section className="item freedom">
-          <img src="https://www.discover.com/credit-cards/images/cards/CardArt_W_Cash_Plum.png" />
+          <img src="https://www.discover.com/credit-cards/images/cards/CardArt_W_Cash_Plum.png" alt="freedom" />
           <ul>
           { Categories.discover.map((category) =>
             <li className="category">{category}</li>
@@ -23,7 +23,7 @@ class App extends Component {
         </ul>
         </section>
         <section className="item cashplus">
-          <img src="https://mycard.usbank.com/credit/mycardusb/html/assets/images/cashplus/card-art-cash-plus.png" />        
+          <img src="https://mycard.usbank.com/credit/mycardusb/html/assets/images/cashplus/card-art-cash-plus.png" alt="cashplus"/>        
             <ul>
               <li className="category">2%: {Categories.cashplus.two}</li>
             { Categories.cashplus.five.map((category) =>
